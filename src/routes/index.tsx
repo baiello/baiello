f3b5b0d284ecc9b85355b5a8e792dea730d119e8
Brice AIELLO <brice.aiello@me.com>
2022-12-27 17:12:02 +0100

    Create and display resume

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,51 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useStyles$ } from '@builder.io/qwik';
+import { Link } from '@builder.io/qwik-city';
 import type { DocumentHead } from '@builder.io/qwik-city';
+import classNames from 'classnames';
 
-import { Welcome } from '~/components/welcome/welcome';
+import { jobs, trainings } from '~/data/resume_en.json';
+
+import styles from './index.css?inline';
 
 export default component$(() => {
+  useStyles$(styles);
+
   return (
-    <Welcome />
+    <section class="container timeline">
+      <h2 class="editing">About me.</h2>
+      <div class="row">
+        <div class="column">
+          <h3>Experience</h3>
+          {
+            jobs.map((job, index) => (
+              <div class={classNames('timeline-item', { marked: job.current })} key={index}>
+                <h4>
+                  {job.position} - {
+                    job.companyUrl
+                      ? <Link href={job.companyUrl} target="_blank" class="company">{job.company}</Link>
+                      : (<span class="company">{job.company}</span>)
+                  }
+                </h4>
+                <div class="period">{job.start} - {job.end} | {job.location}</div>
+                <div class="description">{job.description}</div>
+              </div>
+            ))
+          }
+        </div>
+        <div class="column">
+          <h3>Education</h3>
+          {
+              trainings.map((training, index) => (
+                <div class="timeline-item" key={ index }>
+                  <h4><Link href={training.schoolUrl} target="_blank" class="establishment">{training.schoolName}</Link></h4>
+                  <div class="period">{training.period} | {training.location}</div>
+                  { training.diploma && <div>{training.diploma}</div> }
+                </div>
+              ))
+            }
+        </div>
+      </div>
+    </section>
   );
 });
 
@@ -14,7 +54,7 @@ export const head: DocumentHead = {
   meta: [
     {
       name: 'description',
-      content: 'Brice AIELLO',
+      content: 'Who is Brice AIELLO?',
     },
   ],
 };
